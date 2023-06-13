7edce87d4a674296a906b44ffcc3f1deb97f0a22
unknown <yuravalkivdev@gmail.com>
2023-06-13 16:25:40 +0300

    start developing

---
+++ src/test/java/reactive/gradle/yuravalkiv/ReactiveApp/ReactiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package reactive.gradle.yuravalkiv.ReactiveApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
